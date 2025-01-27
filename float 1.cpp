9033c9100bdbd18fb9aa7e1e34ee8c398640f311
Umair Ahmed Bhutto <bhuttoumairahmed949@gmail.com>
2025-01-27 18:40:15 +0500

    Add files via upload

---
+++ float 1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	float c;
+	cout<<"Enter 1st number"<<a<<endl;
+	cin>>a;
+	cout<<"Enter 2nd number"<<a<<endl;
+	cin>>b;
+	cout<<"Enter 3rd number"<<c<<endl;
+	cin>>c;
+	cout<<a<<endl;
+	cout<<b<<endl;
+	cout<<c;
+
+	return 0;
+}
