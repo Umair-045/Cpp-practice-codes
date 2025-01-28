e2b0f7a73fe6b5d7c8479097864e06ad1cc261dc
Umair Ahmed Bhutto <bhuttoumairahmed949@gmail.com>
2025-01-28 22:03:17 +0500

    Add files via upload

---
+++ Percentage.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int obtained = 934;
+	int total    = 1100;
+	float percentage =(float)obtained/total*100;
+cout<<"Percentage is "<<percentage;
+return 0;
+}
