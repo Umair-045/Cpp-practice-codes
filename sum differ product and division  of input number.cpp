a97c9c3664e50cc12fe2a825d1919a940f6ad677
Umair Ahmed Bhutto <bhuttoumairahmed949@gmail.com>
2025-02-03 11:26:56 +0500

    Add files via upload

---
+++ sum differ product and division  of input number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+int a;
+int b;
+cout<<"Enter numbers";
+cin>>a>>b;
+cout<<"Sum "<<a+b<<endl;
+cout<<"Difference "<<a-b<<endl;
+cout<<"Product "<<a*b<<endl;
+cout<<"Division "<<a/b;
+return 0;
+}
