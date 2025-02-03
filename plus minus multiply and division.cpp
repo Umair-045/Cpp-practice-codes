478a38c3a9054e4ac92b09f0e0f23c408d768b62
Umair Ahmed Bhutto <bhuttoumairahmed949@gmail.com>
2025-02-03 11:24:40 +0500

    Add files via upload

---
+++ plus minus multiply and division.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int a = 10;
+int b = 5;
+int main()
+{
+	cout<<"a+b = "<<a+b<<endl;
+	cout<<"a-b = "<<a-b<<endl;
+	cout<<"a*b = "<<a*b<<endl;
+	cout<<"a/b = "<<a/b;
+	return 0;
+
+}
