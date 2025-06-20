7cda14b4ebd0fb46fc97d90cb8782b2ce5bf2dbe
Umair Ahmed Bhutto <bhuttoumairahmed949@gmail.com>
2025-06-20 11:16:50 +0500

    Add files via upload

---
+++ Reversre.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cout<<"Enter the first value :";
+	cin>>a;
+		cout<<"Enter the second value :";
+		cin>>b;
+	cout<<"Enter the third value :";
+	cin>>c;
+	cout<<c<<endl;
+	cout<<b<<endl;
+	cout<<a<<endl;
+	return 0;
+}
