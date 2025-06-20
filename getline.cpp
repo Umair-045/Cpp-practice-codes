1adb0d24d5f0197ba6e0403fbc06cadd04710f28
Umair Ahmed Bhutto <bhuttoumairahmed949@gmail.com>
2025-06-20 11:16:26 +0500

    Add files via upload

---
+++ getline.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string Name ;
+	string rollno;
+	string section;
+	string institute;
+	cout<<"Name :"<<Name;
+	getline(cin,Name);
+	cout<<"Roll No :"<<rollno;
+	getline(cin,rollno);
+	cout<<"section :"<<section;
+	getline(cin,rollno);
+
+	cout<<"institute :"<<institute;
+	getline(cin,Name);
+return 0;
+
+	
+}
