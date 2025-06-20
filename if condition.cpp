32cbb59281c6817b783feab2558199471493a40b
Umair Ahmed Bhutto <bhuttoumairahmed949@gmail.com>
2025-06-20 14:56:01 +0500

    Add files via upload

---
+++ if condition.cpp
@@ -0,0 +1,212 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	/*int num=-5;
+	cout<<"Enter any number : "<<num<<endl;
+	
+	if(num>0)
+	{
+		cout<<"num is +ve ";
+	}
+	else
+	{
+		cout<<"num is -ve";
+	}*/
+	
+	
+/*	char ch;
+	cout<<"Enter any character :";
+	cin>>ch;
+	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+	{
+	cout<<"character is vowel :";	
+	}
+	else
+	{
+		cout<<"character is consonent";
+	}*/
+	
+	
+/*	int marks;
+	cout<<"Enter the marks";
+	cin>>marks;
+	if(marks>=90)
+	{
+		cout<<"Grade A";
+	}
+	
+	else if (marks>=80)
+{
+		cout<<"Grade B";
+}
+else
+{
+
+cout<<"Grade is C";
+	}*/
+	
+	
+	/*float classesattended,classesheld;
+	cout<<"Classes attended :";
+	cin>>classesattended;
+		cout<<"Classes held :";
+		cin>>classesheld;
+		float percentage = (classesattended/classesheld)*100;
+		if(percentage>=75)
+		{
+			cout<<"Student can sit in exam :";
+		}
+else
+{
+	cout<<"Student cannot sit in exam ";
+}*/
+
+/*int num1, num2;
+cout<<"Enter 1st number : ";
+cin>>num1;
+cout<<"Enter 2nd number : ";
+cin>>num2;
+if(num1==num2)
+{
+	cout<<"Both are equal ";
+	
+}
+else{
+	cout<<"Not equal ";
+	
+}*/
+
+
+/*int num1,num2 ;
+cout<<"Enter 1st number :";
+cin>>num1;
+cout<<"Enter 2nd number :";
+cin>>num2;
+if(num1>num2)
+{
+	cout<<num1<<" is greater than "<<num2;
+	}	
+	else
+	cout<<num1<< "is less than "<<num2;*/
+	
+	
+/*	int num;
+	cout<<"Enter any number :";
+	cin>>num;
+	if(num%2==0)
+	{
+		cout<<"Number is even ";
+	}
+	else
+	{
+	
+	cout<<"Number is odd ";
+}*/
+
+
+
+
+/*char ch;
+cout<<"Enter Character :";
+ch=getch();
+
+cout<<"\nEntered character is :"<<ch;*/
+
+/*char ch;
+cout<<"Enter Character :";
+ch=getche();
+
+cout<<"\nEntered character is :"<<ch;*/
+
+ /* int num1, num2;
+  cout<<"Enter 1st num";
+  cin>>num1;
+  cout<<"Enter 2nd num";
+  cin>>num2;
+  
+  if(num1==num2)
+  {
+  	cout<<"Both are equal";
+  }
+  
+  
+  	else {
+  		cout<<"Not equal";
+	  } */
+	  
+  
+    /*GRADING
+
+		int marks;
+		char a ='A';
+		char b ='B';
+		char c ='C';
+		char d ='D';
+
+		cout<<"Enter Marks : ";
+		cin>>marks;
+		if(marks>=90)
+	{
+		cout<<"Your Grade is : "<<a;
+	}
+	else if(marks>=80){
+		cout<<"Your Grade is : "<<b;
+		
+	}
+	else if (marks>=70){
+	cout<<"Your Grade is : "<<c;
+	}
+	else if (marks>=60)
+	{
+		cout<<"Your Grade is : "<<d;
+	}
+	else {
+		cout<<"You are failed ";
+	}*/
+	
+	
+	
+/*int age ;
+cout<<"Enter your age : ";
+cin>>age;
+if(age>=18&&age<31){
+	cout<<"Go Karachi to vote "<<endl;
+}	
+else if(age>=31&&age<61){
+	cout<<"Your booth  is in hyderabad "<<endl;
+}
+	else if(age>=61){
+		cout<<"you can vote from home "<<endl;
+	}
+	else {
+		cout<<"You are not eligible  to vote ";
+	}*/
+	
+	
+/*int age ;
+cout<<"Enter your age : ";
+cin>>age;
+if(age>=18&&age<31){
+	cout<<"Go Karachi to vote "<<endl;
+}	
+if(age>=31&&age<61){
+	cout<<"Your booth  is in hyderabad "<<endl;
+}
+	if(age>=61){
+		cout<<"you can vote from home "<<endl;
+	}
+	else {
+		cout<<"You are not eligible  to vote ";
+	}*/
+		
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
