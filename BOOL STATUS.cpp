9757f94e5955f8032274cdad684b82263c5d1b3b
Umair Ahmed Bhutto <bhuttoumairahmed949@gmail.com>
2025-06-20 11:15:55 +0500

    Add files via upload

---
+++ BOOL STATUS.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+/*	bool status = true;
+	cout<<status;*/
+/*	bool status = false;
+	cout<<status;*/
+	
+/*	bool status = true;
+cout<<boolalpha<<status;*/
+	
+/*	bool status = false ;
+	cout<<boolalpha<<status;*/
+	
+/*	bool status = false ;
+	cout<<noboolalpha<<status;*/
+	
+/*	bool status = true;
+	cout<<noboolalpha<<status;*/
+	
+	
+	return 0;
+}
